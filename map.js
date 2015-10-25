689711edf35a8821de384d1b718fd5a71c8fcfda
Preksha Koirala <prekshakoirala@gmail.com>
2015-10-24 22:03:33 -0400

    map, sider and input for hashtags

---
+++ map.js
@@ -0,0 +1,14 @@
+function initMap() {
+  var myLatLng = {lat: 38.917064, lng: -77.029029};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
---
+++ random.js
@@ -0,0 +1,6 @@
+
+new Dragdealer('just-a-slider', {
+  animationCallback: function(x, y) {
+    $('#just-a-slider .value').text(Math.round(x * 100));
+  }
+});
